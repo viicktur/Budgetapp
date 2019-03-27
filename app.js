06ce9c29c1f4e7d2672d94ff87da80c77c64c9b8
Okere Victor <okerevictor26@yahoo.com>
2019-03-27 02:34:04 +0100

    add Minox ux fix

--- app.js
+++ app.js
@@ -194,6 +194,12 @@ var UIController = (function() {
             return (type === "exp" ?  "-" : "+") + " " + int + ". " + dec;
         };
 
+        var nodeListForEach = function(list, callback) {
+                for(var i = 0; i < list.length; i++){
+                    callback(list[i], i);
+                }
+            };
+
     return {
         getInput: function() {
 
@@ -270,11 +276,7 @@ var UIController = (function() {
 
 
 
-            var nodeListForEach = function(list, callback) {
-                for(var i = 0; i < list.length; i++){
-                    callback(list[i], i);
-                }
-            };
+            
             nodeListForEach(fields, function(current, index) {
 
                 if(percentages[index] > 0) {
@@ -297,7 +299,19 @@ var UIController = (function() {
 
             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + " " + year;
         },
-        
+        changedType :function() {
+
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + "," +
+                DOMstrings.inputDescription, + "," +
+                DOMstrings.inputValue);
+
+            nodeListForEach(fields, function(cur) {
+
+                cur.classList.toggle("red-focus")
+            });
+            document.querySelector(DOMstrings.inputBtn).classList.toggle("red");
+        },
 
         getDOMstring:function(){
             return DOMstrings;
@@ -323,6 +337,8 @@ var controller = (function(budgetCtrl,UICtrl) {
                 }
             });
         document.querySelector(DOM.container).addEventListener("click",ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener("change",UICtrl.changedType);
     };
 
     var updateBudget = function(){
